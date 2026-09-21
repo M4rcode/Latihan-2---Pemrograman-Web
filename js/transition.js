4086db1eae394f62b5d32e52a910c03a1f7d8507
M4rcode <somethingcaliber@gmail.com>
2026-09-21 17:42:27 +0700

    Pengerjaan 6: Penambahan transition dan javascript

---
+++ js/transition.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    const containers = document.querySelectorAll('.container');
+    containers.forEach(container => observer.observe(container));
+});
